b64f3a57aba2e4adaee3d922d0e06ef538b33ff4
ThinhLe1208 <lebaothinh1208@gmail.com>
2023-02-22 04:15:34 +0700

    Dựng khung layout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,10 @@
+import className from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+
+const cx = className.bind(styles);
+
 function Sidebar() {
-  return <h2>Sidebar</h2>;
+  return <aside className={cx("wrapper")}>Sidebar</aside>;
 }
 
 export default Sidebar;
