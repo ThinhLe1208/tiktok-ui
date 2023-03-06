cd5fb34d1ef020825f13cffab28befc40cdf0535
ThinhLe1208 <lebaothinh1208@gmail.com>
2023-03-06 16:46:33 +0700

    Xây dựng Logic phần Header #3

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -8,6 +8,7 @@ import styles from "./Search.module.scss";
 import { Wrapper as PopperWrapper } from "~/components/Popper";
 import AccountItem from "~/components/AccountItem";
 import { SearchIcon } from "~/components/Icons";
+import { useDebounce } from "~/hooks";
 
 const cx = classNames.bind(styles);
 
@@ -19,22 +20,24 @@ function Search() {
 
   const inputRef = useRef();
 
+  const debounced = useDebounce(searchValue, 500);
+
   useEffect(() => {
-    if (!searchValue.trim()) {
+    if (!debounced.trim()) {
       setSearchResult([]);
       return;
     }
 
     setShowLoading(true);
 
-    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
       .then((res) => res.json())
       .then((res) => {
         setSearchResult(res.data);
         setShowLoading(false);
       })
       .catch(() => setShowLoading(false));
-  }, [searchValue]);
+  }, [debounced]);
 
   const handleClear = () => {
     setSearchValue("");
